ee6a4f05bb4255a6405408abf19aad0faeeedafc
Gabriel Fernella Garcia dos Santos <GabrielFernella@users.noreply.github.com>
2020-04-17 11:51:40 -0300

    Send Mail recovery password update

--- gonode/app/Controllers/Http/ForgotPasswordController.js
+++ gonode/app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,22 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        //No Array adiante vc pode inserir mais de um aquivo somente de texto para o usuário, para evitar que seu dominio ploqueie qualquer tipo de email.
+        ['emails.forgot_password'],
+        //Nas chaves adiante, estamos adicionando as variaveis que passaremos como parametro, o token por exemplo, para ele entrar no formulário já autenticado.
+        //Em redirect_url o front end irá adicionar o endereço do formulário de recuperação de senha
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`},
+        message => {
+        message
+          .to(user.email)
+          .from('teste@gmail.com', 'Email de Teste')
+          .subject(`Recuperação de senha do ${user.username}`)
+      })
 
     } catch (error) {
       return response.status(error.status).send({ error: { message: 'Algo não deu certo, esse e-mail existe?' }})
